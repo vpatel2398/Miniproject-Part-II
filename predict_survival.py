7938d77f7cf3a9d75ca8bef1f9fab95c28ad7591
yash1099 <yashupadhyay10@yahoo.com>
2023-07-14 21:35:39 -0400

    training_the_model

--- predict_survival.py
+++ predict_survival.py
@@ -12,8 +12,6 @@ import streamlit.components.v1 as components
 from PIL import Image
 
 
-
-
 def main():
     img = Image.open('ship.jpg')
 
@@ -29,6 +27,32 @@ def main():
     X = X.values.astype(np.float32)
 
     X_train, X_val, y_train, y_val = train_test_split(X,y, test_size=0.2, random_state=1)
+    model_filename = "island_survival.h5"
+
+    ## Model Training
+def train_model(X_train,y_train):
+    model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(7,)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(1, activation=tf.nn.sigmoid)
+    ])
+
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.00001)
+    model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+    model.fit(X_train, y_train, epochs=25, batch_size=1,validation_data=(X_val, y_val))
+    
+    return model
+
+ # Check if the file already exists
+if os.path.exists(model_filename):
+    # Load the existing model
+    model = load_model(model_filename)
+    print("Existing model loaded.")
+else:
+    # Train and save the model
+    model = train_model(X_train, y_train)
+    model.save(model_filename)
+    print("New model trained and saved.")
 
 if __name__ == '__main__':
     main()
