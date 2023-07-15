1889e4d1768fe5feaed16f13f339627d335525e0
hetvishah10 <122691900+hetvishah10@users.noreply.github.com>
2023-07-14 21:24:57 -0400

    split the data

--- predict_survival.py
+++ predict_survival.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import load_model
@@ -10,14 +10,25 @@ import os
 import streamlit as st
 import streamlit.components.v1 as components
 from PIL import Image
-import pickle
 
 
 
-img = Image.open('ship.jpg')
 
-df = pd.read_csv('island_data.csv')
+def main():
+    img = Image.open('ship.jpg')
 
-#encode the sec column
-df.loc[df['Sex']=='male', 'Sex'] = 1
-df.loc[df['Sex']=='female', 'Sex'] = 0
+    df = pd.read_csv('island_data.csv')
+
+    #encode the sec column
+    df.loc[df['Sex']=='male', 'Sex'] = 1
+    df.loc[df['Sex']=='female', 'Sex'] = 0
+
+    #split the data in to independent x and y variables
+    X = df.drop('Survived', axis =1)
+    y=df['Survived'].values.astype(np.float32)
+    X = X.values.astype(np.float32)
+
+    X_train, X_val, y_train, y_val = train_test_split(X,y, test_size=0.2, random_state=1)
+
+if __name__ == '__main__':
+    main()
